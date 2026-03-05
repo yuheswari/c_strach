e3b4371af3125a76c38ff354c9a2af6b0488fd20
Yuheswari <yuheswari2525@gmail.com>
2026-03-05 12:06:26 +0530

    array

---
+++ arra.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+    int heights[50];
+    heights[0]=30;
+    heights[1]=8;
+    heights[2]=54;
+
+    printf("%d\n",heights[0]);
+    printf("%d\n",heights[1]);
+    printf("%d\n",heights[3]);
+    heights[0]=10; //update
+
+    //traverse 
+    
+
+}
